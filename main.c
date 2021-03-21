47893f3f24eb484d3724c269f52f9a16bea35fd8
Vitor_Mob <69119427+VitorMob@users.noreply.github.com>
2021-03-21 11:12:16 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]){
+	
+	char option; 
+	char pass[100];
+ 
+	do{
+			
+		printf("Crypt :: ");
+		scanf("\n%s", *&pass);
+		
+		puts("\n");
+		char *Crypt = crypt(pass, "lip");
+
+		printf("Pass  :: %s\n", pass);
+		printf("Crypt :: %s\n\n", Crypt);
+		
+		
+		puts("Continue [y/n] :: ");
+		scanf("\n%c", &option);
+		system("clear");
+	}while(option == 'y');
+	
+		if(option == 'n'){
+			puts("Bye");
+		}	
+
+return 0;
+}
