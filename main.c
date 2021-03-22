97c8bad020cd8b9fbce73bd559bc64a7b81654fe
Vitor_Mob <69119427+VitorMob@users.noreply.github.com>
2021-03-22 08:59:54 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
+int main(void){ 
+    char option;
+    char pass[100];
+
+    do{
+
+            printf("Crypt :: ");
+            scanf("\n%s", *&pass);
 
 
-int main(int argc, char *argv[]){
-	
-	char option; 
-	char pass[100];
- 
-	do{
-			
-		printf("Crypt :: ");
-		scanf("\n%s", *&pass);
-		
-		puts("\n");
-		char *Crypt = crypt(pass, "li");
+            puts("\n");
+            char *c = crypt(pass, "li");
+            printf("Pass  :: %s\n", pass);
+            printf("Crypt :: %s\n", c);
+            printf("HexCrypt :: ");
+            for (int i=0; i < strlen(c); i++) {
+                printf("%x", c[i]);
+            }
+            printf("\n");
+                   
+            puts("Continue [y/n] :: ");
+            scanf("\n%c", &option);
+    }while(option == 'y');
 
-		printf("Pass  :: %s\n", pass);
-		printf("Crypt :: $%s\n\n", Crypt);
-		
-		
-		puts("Continue [y/n] :: ");
-		scanf("\n%c", &option);
-		system("clear");
-	}while(option == 'y');
-	
-		if(option == 'n'){
-			puts("Bye");
-		}	
+            if(option == 'n'){
+                    puts("Bye");
+            }
 
-return 0;
+    return 0;
 }
