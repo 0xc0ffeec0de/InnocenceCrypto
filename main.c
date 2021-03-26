b80466e4cf71dcd50dcef4854ffaf3b1c78f5d52
Vitor_Mob <69119427+VitorMob@users.noreply.github.com>
2021-03-26 11:25:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,6 +10,8 @@
 #include <unistd.h>
 #include <string.h>
  
+#include "crypton.h"
+
 #define MALLOC(ptr, size){ \
         ptr = malloc(size); \
                 if (ptr == NULL){ \
@@ -23,47 +25,15 @@ int main(void){
         char option;
         char *pass;
         MALLOC(pass, sizeof(char));
-        char afbt[28] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '$', '&'};
-    
         do{
 
             printf(":: Pass :: ");
             scanf("\n%s", *&pass);
             
             printf("\n:: CryptOM ::"); 
-            printf("MO");
-            for (int i=0; i < strlen(pass); i++) {
-              switch(pass[i]){
-                case('a'): printf("%c", afbt[3]);break; 
-                case('b'): printf("%c", afbt[4]);break;
-                case('c'): printf("%c", afbt[5]);break; 
-                case('d'): printf("%c", afbt[6]);break;  
-                case('e'): printf("%c", afbt[7]);break;  
-                case('f'): printf("%c", afbt[8]);break;  
-                case('g'): printf("%c", afbt[9]);break;  
-                case('h'): printf("%c", afbt[10]);break;  
-                case('i'): printf("%c", afbt[11]);break;  
-                case('j'): printf("%c", afbt[12]);break;  
-                case('k'): printf("%c", afbt[13]);break;  
-                case('l'): printf("%c", afbt[14]);break;  
-                case('m'): printf("%c", afbt[15]);break;  
-                case('n'): printf("%c", afbt[16]);break; 
-                case('o'): printf("%c", afbt[17]);break;  
-                case('p'): printf("%c", afbt[18]);break;  
-                case('q'): printf("%c", afbt[19]);break;  
-                case('r'): printf("%c", afbt[20]);break;  
-                case('s'): printf("%c", afbt[21]);break;  
-                case('t'): printf("%c", afbt[22]);break;  
-                case('u'): printf("%c", afbt[23]);break;  
-                case('v'): printf("%c", afbt[24]);break;  
-                case('w'): printf("%c", afbt[25]);break;  
-                case('x'): printf("%c", afbt[26]);break;  
-                case('y'): printf("%c", afbt[27]);break;  
-                case('z'): printf("%c", afbt[0]);break;  
-                case('$'): printf("%c", afbt[1]);break;  
-                case('&'): printf("%c", afbt[2]);break;  
-              }
-            }
+            printf("$MO");
+            crypton(pass);          
+
             char *c = crypt(pass, "MO");
             printf("\n\n:: CryptDES :: %s\n", c);
 
