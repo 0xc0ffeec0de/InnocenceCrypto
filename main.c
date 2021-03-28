0eb69fd10e1688be88c54407e28d2e5119d6767a
Vitor_Mob <69119427+VitorMob@users.noreply.github.com>
2021-03-28 20:23:00 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+  /*
+ *#codification-UTF-8
+ *#Coded by :: Mob and Onion \._./
  *#Compile :: make main.c main
  *#run :: ./main
  */
@@ -16,8 +19,8 @@ void help(){
 int main(int argc, char *argv[]){
         char *pass = argv[1];
         if(argc >= 2){
-                printf("\n\n|   Cryptom   |   "); 
-                crypton(pass, "MO");printf("   |\n\n");          
+                printf("\n\n|   Hash   |   "); 
+                crypton(pass, "RB");printf("   |\n\n");          
         }else
 
                 help();
