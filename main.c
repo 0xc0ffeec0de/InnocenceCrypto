6648cdabc2ab5cd3be2d9c1d955460e4a20b30a6
Vitor_Mob <69119427+VitorMob@users.noreply.github.com>
2021-03-28 19:37:39 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,48 +4,21 @@
  *#Compile :: gcc -Wall main.c -o main -lcrypt
  *#run :: ./main
  */
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include "crypton.h"
 
-int main(void){
 
-        char option;
-        char *pass = malloc(sizeof(char));
-        if(pass == NULL)
-                fprintf(stderr ,"[!][Error in alocate Memory]");
+int main(int argc, char *argv[]){
+        char *pass = argv[1];
+        if(argc >= 2){
+                printf("|   Cryptom   |   "); 
+                crypton(pass, "MO");printf("   |");          
+        }else
+                 printf("[!][Error not Argument]");
 
-        do{
-
-            printf(":: Pass :: ");
-            scanf("\n%s", *&pass);
-            
-            printf("\n:: Cryptom ::"); 
-            crypton(pass, "MO");          
-
-            char *c = crypt(pass, "MO");
-            printf("\n\n:: CryptDES :: %s\n", c);
-            
-            printf(":: HexCryptDES :: ");
-            for (int i=0; i < strlen(c); i++)
-                printf("%x", c[i]);
-            
-            puts("\n\n[!][Continue? y/n] :: ");
-            scanf("\n%c", &option);
-
-    }while(option == 'y');
-
-            if(option == 'n'){
-                puts("\n[Bye]");
-                exit(1); 
-            }else{
-                    printf("\n[!][Option Invalid][Default return Main]\n\n");
-                    main();
-            }
-    
-        free(pass);
 return 0;
+
 }
+
