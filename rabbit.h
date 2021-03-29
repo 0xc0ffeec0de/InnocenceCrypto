cb915b3484fa42a858e18397ac1a7007e70bdbb1
Vitor_Mob <69119427+VitorMob@users.noreply.github.com>
2021-03-29 17:26:00 -0300

    Update and rename crypton.h to rabbit.h

--- rabbit.h
+++ rabbit.h
@@ -3,7 +3,7 @@
 
 #define afbt "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.!@#$%&*-_<>[]{}"
 
-int crypton(char *pass, char *pr_bytes){
+int rabbit(char *pass, char *pr_bytes){
         for(int i=0;i < strlen(pr_bytes); i++){
                 printf("%c", pr_bytes[i]);
                 }          
