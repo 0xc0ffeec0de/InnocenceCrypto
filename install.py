7e27428cb64ea9f83b8c4adc017b8c8a19754242
Vitor_Mob <69119427+VitorMob@users.noreply.github.com>
2021-04-16 18:44:20 -0300

    Update install.py

--- install.py
+++ install.py
@@ -4,24 +4,40 @@
 #Libs
 import os
 import time
+import PySimpleGUI as sg
 
 def __install__():
+    sg.theme('DarkBlue')
     
-    #Work around :)
-    os.system("make ./bin/main")
-    os.system("pip3 install -r ./libs/requirements.txt")
+    layout = [
+            [sg.Text("Installing")],
+            [sg.ProgressBar(5, orientation='h', size=(30,30), key=('Bar'))],
+            [sg.Output(size=(45,10))],
+            [sg.Cancel()]
+            ]
+
+    window = sg.Window('Installing Dependencies', layout)
+    Bar = window['Bar']
+    
+    for i in range(5):
+        event, values= window.read(timeout=5)
+        print(os.popen("echo Actualizando Packges...").read())
+        time.sleep(0.1)
+        print(os.popen("echo Permission Actualizando...").read())
+        print(os.popen("chmod 755 ./bin/main").read())
+        print(os.popen("chmod 755 rabbit.py").read())
+        time.sleep(0.1)
+        if event == 'Cancel' or event == sg.WIN_CLOSED:
+            sg.popup("Error")
+            break
+        Bar.Update(i+1)
+    print(os.popen("pip3 install -r ./libs/requirements.txt").read())
+    print(os.popen("make ./bin/main").read())
     time.sleep(2)
-    #os.system("rm ./libs/requirements.txt")
-    if os.system("ls ./bin/main") == 0:
-        os.system("chmod 755 ./bin/main")
-        os.system("chmod 755 rabbit.py")
-        print("\nArchive_Compiled")
-    else:
-        print("\nError_Unexpected")
-
-
+    sg.popup("Done")
+    window.close()
+    os.system("./rabbit.py")
 try:
-    __install__()
+   __install__()
 except KeyboardInterrupt:
     print("You_Broke_Off")
-
