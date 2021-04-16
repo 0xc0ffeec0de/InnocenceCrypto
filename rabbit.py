b6a6913b00cca79f0d967691802b8a9fc3615890
Vitor_Mob <69119427+VitorMob@users.noreply.github.com>
2021-04-16 18:43:54 -0300

    Update rabbit.py

--- rabbit.py
+++ rabbit.py
@@ -6,20 +6,26 @@ import PySimpleGUI as sg
 from PySimpleGUI import *
 
 def __init__():
+    
     #Define the window´s contents
-    sg.theme('DarkAmber')
+    sg.theme('DarkBlue')
+    
     layout = [
-            [sg.Image("./assets/rabbit.png")],
-            [sg.Text("Password",size=(10,0)),sg.Input(key='-Passwd-', size=(15,0))],
-            [sg.Button('Encrypt',size=(10,0))],
-            [sg.Text("Password-Hash")],[sg.Output(size=(30,2))],
-            [sg.Button('Quit',size=(10,0))]
+            [sg.Image("./assets/rabbit.png")],#Img-Coelho
+            [sg.Text("Password",size=(10,0), font='Courier 12', background_color='black')],
+            [sg.Input(key='-Passwd-', size=(100,0))],#Input Password
+            [sg.Button('Encrypt',size=(10,0))],#Button-encrypt
+            [sg.Text("Password-Hash",font='Courier 12', background_color='black')],
+            [sg.Output(size=(100,7))],#Output-Hash-Password
+            [sg.Button('Quit',size=(10,0))],#Quit
+            [sg.Image("./assets/grama.png", size=(3000,3000))]#Img-grama
             ]
    
     #Create the Window
-    window = sg.Window("Rabbit-Hash", layout)
+    window = sg.Window("Rabbit-Hash",layout, size=(400, 500), icon=("./assets/rabbit.png"))
+    # Display read
+    
     while True:    
-        # Display read
         event, values = window.read()
         # Exit
         if(event == WIN_CLOSED or event == 'Quit'):
@@ -29,7 +35,6 @@ def __init__():
         # Encrypt 
         if(event == 'Encrypt'):
             print(os.popen(f"./bin/main {Password}").read())
-
 
 
 __init__()
