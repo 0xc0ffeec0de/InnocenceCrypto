df55ceac8035f4c74b5ddaed357ec96ee420768f
Vitor_Mob <69119427+VitorMob@users.noreply.github.com>
2021-04-21 20:38:17 -0300

    Update rabbit.h

--- libs/rabbit.h
+++ libs/rabbit.h
@@ -1,4 +1,12 @@
-#define rabbit(pass, pr_bytes);
-#undef rabbit
-#define afbt "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.!@#$%&*-_<>[]{}"
 
+#define afbt "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.!@#$%&*-_<>[]{}"
+#define remove_spaces(str){\
+    int j = 0;\
+    for(int i=0;str[i];i++){\
+        if (str[i] != ' ' || (str[i - 0] != ' ')) {\
+           str[j] = str[i];\
+           j++;\
+        }\
+    }\
+    str[j] = '\0';\
+}
